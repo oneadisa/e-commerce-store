e9d40dffc90e458809f9ebcb55962c1ca05a7ade
jeffery <jefferyjohnnmose@gmail.com>
2021-11-04 15:38:53 -0700

    tailwind custom colors

--- guage-app/tailwind.config.js
+++ guage-app/tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        "light-blue" : "rgba(245, 248, 255, 1)",
+        "Dark-blue" : "rgba(5, 43, 150, 1)",
+        "medium-blue" : "rgba(5, 49, 173, 1)",
+        "faded-blue" : "rgba(128, 158, 242, 1)",
+      }
+    },
   },
   variants: {
     extend: {},
