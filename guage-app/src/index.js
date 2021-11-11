139f7f0c7eca6bed163d81ebbd720a5ff71c5a78
jeffery <jefferyjohnnmose@gmail.com>
2021-11-11 14:18:57 -0800

    individual another section

--- guage-app/src/index.js
+++ guage-app/src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Campaigns from './componenets/individual-component/Campaigns';
+import ExploreStores from './componenets/individual-component/ExploreStores';
+import ProductsOrders from './componenets/store-components/ProductsOrders';
+import ProductsNew from './componenets/store-components/ProductsNew';
 // import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     
      {/* <App /> */}
-     <Campaigns />
+     {/* <Campaigns /> */}
+     <ExploreStores />
+     {/* <ProductsOrders /> */}
+     {/* <ProductsNew /> */}
   
     </React.StrictMode>
   ,
