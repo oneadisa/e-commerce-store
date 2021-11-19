3be4a6990fa3e2f69b101def6339c23ee5d908a5
jeffery <jefferyjohnnmose@gmail.com>
2021-11-19 11:26:33 -0800

    campaign item pages and others

--- guage-app/src/index.js
+++ guage-app/src/index.js
@@ -10,6 +10,10 @@ import PublishedStore from './componenets/published-component/PublishedStore';
 import SettingsGeneral from './componenets/setting-components/SettingsGeneral';
 import SettingsBank from './componenets/setting-components/SettingsBank';
 import SettingsStore from './componenets/setting-components/SettingsStore';
+import ExploreCampaigns from './componenets/individual-component/ExploreCampaigns';
+import CampaignsFirst from './componenets/individual-component/CampaignsFirst';
+import CampaignsSecond from './componenets/individual-component/CampaignsSecond';
+import CampaignsThird from './componenets/individual-component/CampaignsThird';
 // import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
@@ -23,7 +27,11 @@ ReactDOM.render(
      {/* <PublishedStore /> */}
      {/* <SettingsGeneral /> */}
      {/* <SettingsBank /> */}
-     <SettingsStore />
+     {/* <SettingsStore /> */}
+     {/* <ExploreCampaigns /> */}
+     {/* <CampaignsFirst /> */}
+     {/* <CampaignsSecond /> */}
+     <CampaignsThird />
   
     </React.StrictMode>
   ,
