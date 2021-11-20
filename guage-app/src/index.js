9a79bcccfc22dee758aa1ad1c1ca786286c8fc12
jeffery <jefferyjohnnmose@gmail.com>
2021-11-19 19:55:53 -0800

    cart

--- guage-app/src/index.js
+++ guage-app/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
+import Cart from './componenets/cart-component/Cart';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-     <App />
+    <React.StrictMode>
+     {/* <App /> */}
+      <Cart />
+
     </React.StrictMode>
   ,
   document.getElementById('root')
