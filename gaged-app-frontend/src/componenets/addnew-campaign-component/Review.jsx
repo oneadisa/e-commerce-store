d7bdaf54d92f048a5fd32735584e7ed3954cab2f
jeffery <jefferyjohnnmose@gmail.com>
2021-12-11 21:43:30 -0800

    need fix

--- gaged-app-frontend/src/componenets/addnew-campaign-component/Review.jsx
+++ gaged-app-frontend/src/componenets/addnew-campaign-component/Review.jsx
@@ -1,108 +1,18 @@
 import React, { useState } from "react";
-import Logof from "../../images/logo-footer.jpg";
-import analytics from "../../images/analytics.png";
-import wallet from "../../images/wallet.png";
-import store from "../../images/store.png";
-import settings from "../../images/settings.png";
-import dashboard from "../../images/dashboard.png";
-import campaign from "../../images/campaign.png";
 import video from "../../images/embeddedvideo.svg";
 import right from "../../images/right.svg";
+import Header from "./Header";
+import DashboardCamp from "./DashboardCamp";
 import { Link } from "react-router-dom";
 
 function Review() {
-  const [open, setOpen] = useState(false);
 
-  const handleNav = () => {
-    setOpen(!open);
-  };
-
-<<<<<<< HEAD
-    const handleNav = () => {
-        setOpen(!open);
-    };
-
-
-    return (
-        <div className="mx-auto">
-            <head className="flex justify-between bg-medium-blue px-2 md:px-4 lg:px-4 py-2">
-                <div className="flex space-x-5 lg:space-x-36 h-10">
-                    <div className="flex">
-                        <div className="text-2xl lg:hidden text-white" onClick={handleNav}>
-                        {open === true ? (
-                            <i class="fas fa-times"></i>
-                        ) : (
-                            <i class="fas fa-bars"></i>
-                        )}
-=======
   return (
     <div className="mx-auto">
-      <head className="flex justify-between bg-medium-blue px-2 md:px-4 lg:px-4 py-2">
-        <div className="flex space-x-5 lg:space-x-36 h-10">
-          <div className="flex">
-            <div className="text-2xl lg:hidden text-white" onClick={handleNav}>
-              {open === true ? (
-                <i class="fas fa-times"></i>
-              ) : (
-                <i class="fas fa-bars"></i>
-              )}
-            </div>
-            <img alt="" src={Logof} className="w-4/5 lg:w-full" />
-          </div>
-          <input className="w-36 md:w-64 lg:w-96 outline-none pl-1 lg:pl-5" />
-        </div>
-        <div className="flex gap-2 lg:gap-10 lg:pr-28 ml-auto">
-          <div>
-            <p className="text-xl font-normal text-white mt-1">Cart</p>
-          </div>
-          <div className="w-4 h-4 lg:w-7 lg:h-7 rounded-full bg-white mt-3 lg:mt-1" />
-        </div>
-      </head>
+      <Header />
       <div className="lg:bg-magenta-blue lg:px-4">
         <div className="block lg:flex lg:space-x-32">
-          <ul
-            className={
-              "w-2/3 lg:w-32 min-h-screen lg:min-h-0 absolute transition duration-200 bg-magenta-blue text-Dark-grey lg:static flex flex-col text-lg font-semibold lg:mt-14 lg:block" +
-              (open === true ? " " : " hidden ")
-            }
-          >
-            <li className="mt-0 mb-3 pt-5 lg:pt-0 p-3">
-              <Link to="/" className="flex gap-3">
-                <img alt="" src={dashboard} className="w-5 h-5 mt-1" />
-                <p>Dashboard</p>
-              </Link>
-            </li>
-            <li className="my-3 bg-white p-3 rounded text-Dark-blue mr-10 lg:-mr-10">
-              <Link to="/" className="flex gap-3">
-                <img alt="" src={campaign} className="w-5 h-5 mt-1" />
-                <p>Campaigns</p>
-              </Link>
-            </li>
-            <li className="my-3 p-3">
-              <Link to="/" className="flex gap-3">
-                <img alt="" src={store} className="w-5 h-5 mt-1" />
-                <p>Store</p>
-              </Link>
-            </li>
-            <li className="my-3 p-3">
-              <Link to="/" className="flex gap-3">
-                <img alt="" src={analytics} className="w-5 h-5 mt-1" />
-                <p>Analytics</p>
-              </Link>
-            </li>
-            <li className="my-3 p-3">
-              <Link to="/" className="flex gap-3">
-                <img alt="" src={wallet} className="w-5 h-5 mt-1" />
-                <p>Wallet</p>
-              </Link>
-            </li>
-            <li className="my-3 p-3">
-              <Link to="/" className="flex gap-3">
-                <img alt="" src={settings} className="w-5 h-5 mt-1" />
-                <p>Settings</p>
-              </Link>
-            </li>
-          </ul>
+          <DashboardCamp />
           <div className="bg-white lg:mt-3 lg:mb-8 pb-5 lg:w-3/4">
             <div className="flex flex-col px-2 md:px-4 py-2">
               <h2 className="text-lg font-semibold">6 of 6</h2>
@@ -136,7 +46,6 @@ function Review() {
                       <div className="mt-5 pl-1 flex flex-col gap-6">
                         <div className="w-full h-1 bg-Dark-grey rounded-full">
                           <div className="w-2/6 h-full bg-green-500 rounded-full" />
->>>>>>> main
                         </div>
                         <button className="ml-1 text-base font-medium border-2 border-gray-400 w-28 h-10 hover:bg-Dark-blue hover:border-Dark-blue hover:text-white">
                           Edit
